b0abebcca59c93d57110c633f48c47de578cc2c8
local <local@host>
2024-12-25 22:35:20 +0000

    feat: change hello android gnustl_static=> c++_static

--- projects/hello/proj.android-studio/app/jni/Application.mk
+++ projects/hello/proj.android-studio/app/jni/Application.mk
@@ -1,4 +1,5 @@
-APP_STL := gnustl_static
+# APP_STL := gnustl_static
+APP_STL := c++_static
 
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1  -fsigned-char -std=c++11
 APP_LDFLAGS := -latomic -landroid
